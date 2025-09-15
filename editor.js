61bc9580815604109f126571c554cb43594eb366
marcusarmento <57120916+marcusarmento@users.noreply.github.com>
2025-09-15 07:56:04 -0300

    Replace TinyMCE with simple built-in editor

--- editor.js
+++ editor.js
@@ -1,11 +1,8 @@
-document.addEventListener('DOMContentLoaded', function() {
-    
-    tinymce.init({
-        selector: '#editor-de-texto',
-        height: '100%', // Ocupa a altura do container
-        menubar: false,
-        plugins: 'advlist autolink lists link image charmap print preview anchor searchreplace visualblocks code fullscreen insertdatetime media table paste code help wordcount',
-        toolbar: 'undo redo | formatselect | bold italic backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help'
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('.toolbar button').forEach((button) => {
+        button.addEventListener('click', () => {
+            const command = button.dataset.command;
+            document.execCommand(command, false, null);
+        });
     });
-
 });
